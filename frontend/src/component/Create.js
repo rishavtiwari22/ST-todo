b6b0dcb1ef7d1a61ad7e170aec6b641b9755bb33
rishavtiwari22 <tiwaririshav463@gmail.com>
2024-12-19 16:37:33 +0530

    Update Create.js

--- frontend/src/component/Create.js
+++ frontend/src/component/Create.js
@@ -23,7 +23,7 @@ function Create() {
 
     const todos = { title }; // Prepare the todo to send
 
-    const response = await fetch('http://localhost:5000/', {
+    const response = await fetch('https://st-todo.vercel.app/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
