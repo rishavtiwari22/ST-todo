af2329a1c98c2bae90848aab52600607bdcff585
rishavtiwari22 <tiwaririshav463@gmail.com>
2024-12-19 16:27:04 +0530

    Update Create.js

--- frontend/src/component/Create.js
+++ frontend/src/component/Create.js
@@ -8,7 +8,7 @@ function Create() {
 
   // Function to fetch the todo list from the server
   const getData = async () => {
-    const response = await fetch('http://localhost:5000');
+    const response = await fetch('https://st-todo.vercel.app/');
     const result = await response.json();
     setData(result);
   };
